15dc5b1f7c0262daefcd3a6fb34de87fc99c71e7
OEbinisa <o.ebinisa@gmail.com>
2025-07-26 20:08:43 +0100

    0203 code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
 import TaskList from './components/TaskList'
+import TaskManager from './components/TaskManager';
 
-const sampleTasks = [
-  {id: 1, text: "Complete React Tutorial"},
-  {id: 2, text: "Build a To-Do app"},
-  {id: 3, text: "Learn NextJS"},
-];
+// const sampleTasks = [
+//   {id: 1, text: "Complete React Tutorial"},
+//   {id: 2, text: "Build a To-Do app"},
+//   {id: 3, text: "Learn NextJS"},
+// ];
 
 function App() {
   return (
     <>
       <h1>Task Manager App</h1>
-      <TaskList tasks={sampleTasks} />
+      <TaskManager />
     </>
   );
 }
